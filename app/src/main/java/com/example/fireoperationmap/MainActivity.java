9ced94f1bfe8163dd0219d16c5087be1a6aae3f9
Geunho Kim <kk41912000@naver.com>
2021-02-18 17:57:53 +0900

    상태바 색상 변경

--- app/src/main/java/com/example/fireoperationmap/MainActivity.java
+++ app/src/main/java/com/example/fireoperationmap/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
