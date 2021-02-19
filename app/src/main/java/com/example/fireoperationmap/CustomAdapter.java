e75f9b3f9326eb38bca7232a7fb4255733688f2c
Geunho Kim <kk41912000@naver.com>
2021-02-19 22:27:15 +0900

    검색 필터 설정

--- app/src/main/java/com/example/fireoperationmap/CustomAdapter.java
+++ app/src/main/java/com/example/fireoperationmap/CustomAdapter.java
@@ -1,78 +1,128 @@
 package com.example.fireoperationmap;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CustomAdapter extends FirebaseRecyclerAdapter<User, CustomAdapter.CustomViewHolder> {
-
-    private OnPersonItemClickLister mlistener;
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> implements Filterable {
+    private final List<User> userList = new ArrayList<>();
+    private final List<User> userListFull = new ArrayList<>();
+    private CustomAdapter.OnPersonItemClickLister listener;
+    private String searchState = "st_name";
 
     public interface OnPersonItemClickLister{
-        public void onItemClick(View view, int position);
+        void onItemClick(View view, int position);
     }
 
-    public void setOnItemClickListener(OnPersonItemClickLister listener){
-        this.mlistener = listener;
+    public void setOnItemClickListener(CustomAdapter.OnPersonItemClickLister listener){
+        this.listener = listener;
     }
 
-    public class CustomViewHolder extends RecyclerView.ViewHolder{
-        TextView id, st_name, address, structure, floor, st_type, facility, person, phone1, phone2;
-
-        public CustomViewHolder(@NonNull View itemView) {
-            super(itemView);
-            id = (TextView)itemView.findViewById(R.id.id);
-            address = (TextView)itemView.findViewById(R.id.address);
-            st_name = (TextView)itemView.findViewById(R.id.st_name);
-            structure = (TextView)itemView.findViewById(R.id.structure);
-            floor = (TextView)itemView.findViewById(R.id.floor);
-            st_type = (TextView)itemView.findViewById(R.id.st_type);
-            facility = itemView.findViewById(R.id.facility);
-            person = (TextView)itemView.findViewById(R.id.person);
-            phone1 = (TextView)itemView.findViewById(R.id.phone1);
-            phone2 = (TextView)itemView.findViewById(R.id.phone2);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION){
-                        mlistener.onItemClick(view, position);
+    private final Filter customFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<User> filteredList = new ArrayList<>();
+            if(constraint == null|| constraint.length() == 0){
+                filteredList.addAll(userListFull);
+            }
+            else{
+                String pattern = constraint.toString().toLowerCase().trim();
+                for(User item : userListFull){
+                    if (searchState.equals("st_name") && item.getSt_name().toLowerCase().contains(pattern)){
+                        filteredList.add(item);
                     }
+                    else if (searchState.equals("address") && item.getAddress().toLowerCase().contains(pattern)){
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            userList.clear();
+            userList.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    @Override
+    public Filter getFilter() {
+        return customFilter;
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+        TextView id, st_name, address, structure, floor, st_type, facility, person, phone1, phone2;
+        CustomViewHolder(View itemView) {
+            super(itemView);
+            id = itemView.findViewById(R.id.id);
+            address = itemView.findViewById(R.id.address);
+            st_name = itemView.findViewById(R.id.st_name);
+            structure = itemView.findViewById(R.id.structure);
+            floor = itemView.findViewById(R.id.floor);
+            st_type = itemView.findViewById(R.id.st_type);
+            facility = itemView.findViewById(R.id.facility);
+            person = itemView.findViewById(R.id.person);
+            phone1 = itemView.findViewById(R.id.phone1);
+            phone2 = itemView.findViewById(R.id.phone2);
+
+            itemView.setOnClickListener(view -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION){
+                    listener.onItemClick(view, position);
                 }
             });
         }
     }
 
-    public CustomAdapter(@NonNull FirebaseRecyclerOptions<User> options) {
-        super(options);
-    }
-
     @NonNull
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        return new CustomViewHolder(view);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new CustomViewHolder(v);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull CustomViewHolder holder, int position, @NonNull User model) {
-        holder.id.setText(model.getTagId());
-        holder.address.setText(model.getTagAddress());
-        holder.floor.setText(model.getTagFloor());
-        holder.st_name.setText(model.getTagSt_name());
-        holder.structure.setText(model.getTagStructure());
-        holder.st_type.setText(model.getTagSt_type());
-        holder.facility.setText(model.getTagFaclility());
-        holder.person.setText(model.getTagPerson());
-        holder.phone1.setText(model.getTagPhone1());
-        holder.phone2.setText(model.getTagPhone2());
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        User currentItem = userList.get(position);
+        holder.id.setText(currentItem.getTagId());
+        holder.address.setText(currentItem.getTagAddress());
+        holder.floor.setText(currentItem.getTagFloor());
+        holder.st_name.setText(currentItem.getTagSt_name());
+        holder.structure.setText(currentItem.getTagStructure());
+        holder.st_type.setText(currentItem.getTagSt_type());
+        holder.facility.setText(currentItem.getTagFaclility());
+        holder.person.setText(currentItem.getTagPerson());
+        holder.phone1.setText(currentItem.getTagPhone1());
+        holder.phone2.setText(currentItem.getTagPhone2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public User getItem(int position) {
+        return userList.get(position);
+    }
+
+    public void addUser(User user) {
+        this.userListFull.add(user);
+    }
+
+    public void setSearchState(String searchState) {
+        this.searchState = searchState;
     }
 }
--- app/src/main/java/com/example/fireoperationmap/MainActivity.java
+++ app/src/main/java/com/example/fireoperationmap/MainActivity.java
@@ -1,44 +1,35 @@
 package com.example.fireoperationmap;
 
-import androidx.annotation.RequiresApi;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.github.chrisbanes.photoview.OnMatrixChangedListener;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     private CustomAdapter adapter;
     private RecyclerView recyclerView;
     private SlidingUpPanelLayout slidingUpPanelLayout;
-    private String searchState = "st_name";
     private PhotoView photoView;
 
     @Override
@@ -51,85 +42,92 @@ public class MainActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.hide();
 
-        recyclerView = findViewById(R.id.recyclerView);
         slidingUpPanelLayout = findViewById(R.id.sliding_layout);
 
         createSearchView();
         createMapView();
-        initializeAdapter();
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        initializeAdapterAndRecyclerView();
+    }
+
+    private void initializeAdapterAndRecyclerView() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("User");
+        adapter = new CustomAdapter();
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //파에어베이스에서 adapter.userList로 데이터를 불러옴
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren()) {
+                    User user = data.getValue(User.class);
+                    adapter.addUser(user);
+                }
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //아이템 클릭시 이벤트 설정
+        adapter.setOnItemClickListener((view, position) -> {
+            User user = adapter.getItem(position);
+            Toast.makeText(getApplicationContext(), user.getId() + "가 선택됨", Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void createSearchView() {
-        EditText searchField = (EditText) findViewById(R.id.searchField);
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        ImageButton searchButton = (ImageButton) findViewById(R.id.searchButton);
+        EditText searchField = findViewById(R.id.searchField);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        ImageButton searchButton = findViewById(R.id.searchButton);
 
         radioGroup.check(R.id.rb_stname);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.rb_stname) {
-                    Toast.makeText(MainActivity.this, "상호명으로 검색", Toast.LENGTH_SHORT).show();
-                    searchState = "st_name";
-                }
-                else if (checkedId == R.id.rb_address) {
-                    Toast.makeText(MainActivity.this, "주소지로 검색", Toast.LENGTH_SHORT).show();
-                    searchState = "address";
-                }
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.rb_stname) {
+                Toast.makeText(MainActivity.this, "상호명으로 검색", Toast.LENGTH_SHORT).show();
+                adapter.setSearchState("st_name");
+            }
+            else if (checkedId == R.id.rb_address) {
+                Toast.makeText(MainActivity.this, "주소지로 검색", Toast.LENGTH_SHORT).show();
+                adapter.setSearchState("address");
             }
         });
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String searchText = searchField.getText().toString();
-                if (searchText.equals("")) return;
+        searchButton.setOnClickListener(view -> {
+            InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            //여기 수정 해야함 manger가 불러와졌을때만 적용할 수 있도록
+            //manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            //slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+        });
+
+        searchField.setOnEditorActionListener((view, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
                 manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                processSearch(searchText);
                 slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+                return true;
             }
+
+            return false;
         });
 
-        searchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        searchField.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    String searchText = searchField.getText().toString();
-                    InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                    processSearch(searchText);
-                    slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-                    return true;
-                }
-
-                return false;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
-        });
-    }
 
-    private void processSearch(String searchText) {
-        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
-                .setQuery(FirebaseDatabase.getInstance().getReference().child("User")
-                        .orderByChild(searchState).startAt(searchText).endAt(searchText + "\uf8ff"), User.class).build();
-
-        adapter.updateOptions(options);
-        adapter.startListening();
-        recyclerView.setAdapter(adapter);
-    }
-
-    private void initializeAdapter() {
-        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>().setQuery(database.getReference().child("User"), User.class).build();
-        adapter = new CustomAdapter(options);
-
-        adapter.setOnItemClickListener(new CustomAdapter.OnPersonItemClickLister() {
-            @RequiresApi(api = Build.VERSION_CODES.Q)
             @Override
-            public void onItemClick(View view, int position) {
-                User user = adapter.getItem(position);
-                Toast.makeText(getApplicationContext(), user.getId() + "가 선택됨", Toast.LENGTH_SHORT).show();
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
     }
@@ -141,23 +139,6 @@ public class MainActivity extends AppCompatActivity {
         photoView.setMaximumScale(3.0f);
 
         //테스트 용 좌표 찍기
-        photoView.setOnMatrixChangeListener(new OnMatrixChangedListener() {
-            @Override
-            public void onMatrixChanged(RectF rect) {
-                Log.d("rect", "left: " + rect.left + ", top: " + rect.top);
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        adapter.stopListening();
+        photoView.setOnMatrixChangeListener(rect -> Log.d("rect", "left: " + rect.left + ", top: " + rect.top));
     }
 }
